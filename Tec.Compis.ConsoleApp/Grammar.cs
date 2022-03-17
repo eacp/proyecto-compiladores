3f26d8697634a933e3cac5f02afa013b0da03237
Eduardo Andrés Castillo Perera <eduardoandrescp@outlook.com>
2022-03-17 12:47:50 -0600

    Runs on linux

---
+++ Tec.Compis.ConsoleApp/Grammar.cs
@@ -0,0 +1,46 @@
+﻿namespace Tec.Compis.ConsoleApp;
+
+class Grammar
+{
+    // A grammar has one or more definitions
+    private List<Definition> definitions = new(64);
+
+    /// <summary>
+    /// Add a definition to the grammar based on a line
+    /// </summary>
+    /// <param name="line"></param>
+    internal void Add(string line)
+    {
+        definitions.Add(new Definition(line));
+    }
+}
+
+class Definition
+{
+    /// <summary>
+    /// Name is the "left" part of the definition.
+    /// </summary>
+    internal string Name { get; private set; }
+
+    /// <summary>
+    /// The part on the right
+    /// </summary>
+    private string[] tokens;
+
+    /// <summary>
+    /// Creates a new definition based on a line
+    /// </summary>
+    /// <param name="line">
+    /// The text that makes this definition.
+    /// Example: 'id -> a b c'
+    /// </param>
+    public Definition(string line)
+    {
+        string[] elements = line.Replace('\'', ' ')
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        // Get the ID and the sub array
+        Name = elements[0];
+        tokens = elements.Skip(2).ToArray();
+    }
+}
--- Tec.Compis.ConsoleApp/Program.cs
+++ Tec.Compis.ConsoleApp/Program.cs
@@ -1,5 +1,42 @@
+using Tec.Compis.ConsoleApp;
+
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Diseño de compiladores");
 Console.WriteLine("Eduardo Andrés Castillo Perera - A01702948");
 Console.WriteLine("Profesor: Dr. Pedro Oscar Pérez Murueta");
 Console.WriteLine("El PWD es: " + Directory.GetCurrentDirectory());
+
+// Get the input file
+string filepath = args.Length > 0 ? 
+    args[0] : 
+    ask("Please provid the filepath for the grammar");
+
+// Open the file
+using var grammarFile = File.OpenText(filepath);
+
+
+// The grammar
+Grammar g = new();
+
+// Append all the lines
+// Read the first one
+grammarFile.ReadLine();
+
+while(!grammarFile.EndOfStream)
+{
+    var line = grammarFile.ReadLine();
+    g.Add(line);
+}
+
+// The grammar has been loaded into memory
+// Now execute the algorithm
+
+/// <summary>
+/// Prints a message to the console and reads a line
+/// from the console.
+/// </summary>
+string ask(string message, string defaultAnswer="")
+{
+    Console.WriteLine("Please provide the path of the input file");
+    return Console.ReadLine() ?? defaultAnswer;
+}
